0975acb75b9798b14a857b87dd226093a2baefd6
yossu-y <yangyuub@gmail.com>
2022-04-27 05:58:13 +0000

    [update]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     get "followers" => 'relationships#followers',as: "followers"
   end
 
-  get "articles/search"
+  get "search" => "searches#search",as: "search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
